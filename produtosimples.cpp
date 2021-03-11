8b982619cc2867659ebde1f29f14686fce7fe19c
RafaRLV <77854625+RafaRlv@users.noreply.github.com>
2021-03-11 09:49:43 -0300

    Add files via upload

---
+++ produtosimples.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x=0, y=0, PROD=0;
+    
+    cin >> x;
+    cin >> y;
+    
+    PROD = x * y;
+    
+    cout << "PROD = " << PROD << "\n";
+    
+    
+    return 0;
+}
