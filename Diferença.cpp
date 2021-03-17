85b64dabf45c95723788303385b93ab37e6e613f
RafaRLV <77854625+RafaRlv@users.noreply.github.com>
2021-03-17 11:38:21 -0300

    Add files via upload

---
+++ Diferença.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    int A, B, C, D, dif;
+    
+    cin >> A >> B >> C >> D;
+    
+    dif = (A*B-C*D);
+    
+    cout << "DIFERENCA = " << dif << "\n"; 
+    
+    return 0;
+}
